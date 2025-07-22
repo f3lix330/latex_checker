4257c65d2a491f9286d7f071276a1314cbb3c495
Felix Caesar <felixcaesar2002@gmail.com>
2025-07-22 20:05:28 +0000

    added virtual terminal processing for windows

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ use std::{env, fs};
 use walkdir::WalkDir;
 use colored::Colorize;
 
+#[cfg(windows)]
+fn enable_virtual_terminal_processing() {
+    use winapi_util::console::Console;
+
+    if let Ok(mut term) = Console::stdout() {
+        let _ = term.set_virtual_terminal_processing(true);
+    }
+    if let Ok(mut term) = Console::stderr() {
+        let _ = term.set_virtual_terminal_processing(true);
+    }
+}
+
 fn main() {
+    #[cfg(windows)]
+    enable_virtual_terminal_processing();
+    
     match env::args().nth(1) {
         Some(wd) => {
             let files = get_all_files(PathBuf::from(wd));
